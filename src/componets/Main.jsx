18da8451d78f2151c5e323a796eab6d86e39fce7
Mufaro1410 <fungurayimufaro@gmail.com>
2024-12-11 11:52:53 +0200

    added smooth scroll to recipe when rendered

--- src/componets/Main.jsx
+++ src/componets/Main.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ClaudeRecipe from "./ClaudeRecipe";
 import IngredientsList from "./IngredientsList";
 import IngredientsForm from "./IngredientsForm";
 import { getRecipeFromMistral } from "../ai";
+import { useEffect } from "react";
 
 export default function Main() {
     const [ingredients, setIngredients ] = useState([])
     const [recipe, setRecipe ] = useState("")
+    const recipeSection = useRef(null)
     
     function handleSubmit(event) {
         event.preventDefault()
@@ -14,6 +16,12 @@ export default function Main() {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngre => [...prevIngre, newIngredient])
     }
+
+    useEffect(() => {
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipe])
     
     async function getRecipe() {
         let token = import.meta.env.VITE_HF_ACCESS_TOKEN
@@ -29,7 +37,7 @@ export default function Main() {
     return (
         <main>
             <IngredientsForm handleSubmit={handleSubmit} />
-            {ingredients.length > 0 && <IngredientsList ingredients={ingredients} getRecipe={getRecipe}/>}
+            {ingredients.length > 0 && <IngredientsList ingredients={ingredients} getRecipe={getRecipe} ref={recipeSection}/>}
             {recipe && <ClaudeRecipe recipe={recipe} />}
         </main>
     )
